61ca5f580271d175c81f6589e9df4c89bd9331aa
zeng83 <zhaofeng.zeng83@gmail.com>
2013-10-29 11:13:32 -0700

    add security check before toggle completion

--- togglecompletionlib.php
+++ togglecompletionlib.php
@@ -9,17 +9,15 @@ $moduleid = optional_param('cmid', 0, PARAM_INT);
 
 $userid = $USER->id;
 
-// if (!$cm = get_coursemodule_from_id('quiz', $moduleid)) {
-//     print_error('invalidcoursemodule');
-// }
-// if (!$course = $DB->get_record('course', array('id' => $cm->course))) {
-//     print_error('coursemisconf');
-// }
+if (!$cm = get_coursemodule_from_id('quiz', $moduleid)) {
+    print_error('invalidcoursemodule');
+}
+if (!$course = $DB->get_record('course', array('id' => $cm->course))) {
+    print_error('coursemisconf');
+}
 
-// // Check login and get context.
-// require_login($course, false, $cm);
-// $context = context_module::instance($cm->id);
-// require_capability('mod/quiz:view', $context);
+// Check login and get context.
+require_login($course, false, $cm);
 
 /* Updating quiz completion */
 function toggle_quiz_completion($userid,$moduleid) {
